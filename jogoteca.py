82116e016ab0a99cd54ef86b20571a26f164fe90
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-24 18:45:26 -0300

    xxx

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 
 def ola():
-    return 'Olá Mundo!'
+    return render_template('lista.html')
 
 app.run()
